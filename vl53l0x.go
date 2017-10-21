e6ca0aa177f78ab449b7539f2281da8353ae0e40
1lann <me@chuie.io>
2017-10-21 12:34:08 +1100

    Fix name of sensor

--- vl53l0x.go
+++ vl53l0x.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kidoman/embd"
 )
 
-// Constants used for VL54L0X driver
+// Constants used for VL53L0X driver
 const (
 	VL53L0XAddress = 0x29
 )
@@ -18,15 +18,15 @@ var (
 	ErrOutOfBounds    = errors.New("vl53l0x: measurement out of bounds")
 )
 
-// VL34L0XDriver represents the I2C driver for the VL34L0X proximity chip.
-type VL34L0XDriver struct {
+// VL530LXDriver represents the I2C driver for the VL530LX proximity chip.
+type VL530LXDriver struct {
 	bus     embd.I2CBus
 	address byte
 }
 
-// NewDriver returns a new VL34L0X driver and starts it on the provided I2C bus.
-func NewDriver(bus embd.I2CBus) *VL34L0XDriver {
-	d := &VL34L0XDriver{
+// NewDriver returns a new VL530LX driver and starts it on the provided I2C bus.
+func NewDriver(bus embd.I2CBus) *VL530LXDriver {
+	d := &VL530LXDriver{
 		bus:     bus,
 		address: VL53L0XAddress,
 	}
@@ -35,7 +35,7 @@ func NewDriver(bus embd.I2CBus) *VL34L0XDriver {
 }
 
 // Measure measures the distance detected by the driver.
-func (d *VL34L0XDriver) Measure() (int, error) {
+func (d *VL530LXDriver) Measure() (int, error) {
 	byteA, err := d.bus.ReadByteFromReg(d.address, 0x1E)
 	if err != nil {
 		return 0, err
